3747e8c6942247d318614acf48c8110d999c4d81
nyg7 <yuricantox@gmail.com>
2026-04-27 11:21:56 -0300

    need API

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let startTime, endTime;
-let imageSize ="";
+let imageSize = "";
 let image = new Image();
 let bitSpeed = document.getElementById("bits"),
     kbSpeed = document.getElementById("kbs"),
@@ -9,26 +9,26 @@ let bitSpeed = document.getElementById("bits"),
 let totalBitSpeed = 0;
 let totalKbSpeed = 0;
 let totalMbSpeed = 0;
-let numTests = 5;
-let TestCompleted = 0;
+let numTests = 1;
+let testCompleted = 0;
 
-// Get random image form unsplash.com
-let imageApi = "https://source.unsplash.com/random?topic=nature";
+// Get random image from unsplash.com
+let imageApi = "https://source.unsplash.com/pt-br/fotografias/arranha-ceus-modernos-iluminados-a-noite-com-vista-para-uma-baia-cvXboLY98Go";
 
 // When image loads
-image.onload = async function(){
+image.onload = async function () {
     endTime = new Date().getTime();
 
     // Get image size
-    await fetch(imageApi).then((resp) =>{
-        imageSize = response.headers.get("content-length")
+    await fetch(imageApi).then((response) => {
+        imageSize = response.headers.get("content-length");
         calculateSpeed();
     });
 };
 
 // Function to calculate speed
-function calculateSpeed(){
-    // Time taken in seconds 
+function calculateSpeed() {
+    // Time taken in seconds
     let timeDuration = (endTime - startTime) / 1000;
     // Total bits
     let loadedBits = imageSize * 8;
@@ -40,19 +40,36 @@ function calculateSpeed(){
     totalKbSpeed += speedInKbs;
     totalMbSpeed += speedInMbs;
 
-    TestCompleted++;
+    testCompleted++;
 
-    //If all tests completed (we get 5 image then calculate average)
-    if(TestCompleted === numTests){
+    // If all tests completed (we get 5 image then calculate average)
+    if (testCompleted === numTests) {
         let averageSpeedInBps = (totalBitSpeed / numTests).toFixed(2);
         let averageSpeedInKbps = (totalKbSpeed / numTests).toFixed(2);
         let averageSpeedInMbps = (totalMbSpeed / numTests).toFixed(2);
-    
 
         // Display average speeds
         bitSpeed.innerHTML += `${averageSpeedInBps}`;
         kbSpeed.innerHTML += `${averageSpeedInKbps}`;
         mbSpeed.innerHTML += `${averageSpeedInMbps}`;
-        info.innerHTML = "test Completed";
+        info.innerHTML = "Test Completed!";
+    } else {
+        // Run the next test
+        startTime = new Date().getTime();
+        image.src = imageApi;
     }
 }
+
+// Initial function to start tests
+const init = async () => {
+    info.innerHTML = "Testing...";
+    startTime = new Date().getTime();
+    image.src = imageApi;
+};
+
+// Run tests when window loads
+window.onload = () => {
+    for (let i = 0; i < numTests; i++) {
+        init();
+    }
+};
